c193336c574e2c3ecdd226e4266fc73dc6340636
lbj4312 <137462948+lbj4312@users.noreply.github.com>
2023-07-11 13:07:13 +0800

    Update hq.py

--- hq.py
+++ hq.py
@@ -1,4 +1,1 @@
-import requests
-ip=requests.get("http://6.ipw.cn/").text
-with open("i.html",'w') as f:
-    f.write("<a href='http://["+ip+"]:8000/' target=_blank><button>狗屁不通文章生成器</button></a>\n<a href='http://["+ip+"]:8500/' target=_blank><button>打篮球游戏</button></a>")
+
